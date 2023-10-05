c6a619c1fe192bb93a3bb54c87f40c79d0cc6713
saku1331 <sakshikhokhar73@gmail.com>
2023-10-05 20:37:28 +0530

    Cat Game added

---
+++ Sample/CatGame/script.js
@@ -0,0 +1,14 @@
+const catSelect = document.getElementById('cat-select');
+        const memeTextInput = document.getElementById('meme-text-input');
+        const generateButton = document.getElementById('generate-button');
+        const catImage = document.getElementById('cat-image');
+        const memeText = document.getElementById('meme-text');
+        
+        // Function to generate the meme
+        generateButton.addEventListener('click', () => {
+            const selectedCat = catSelect.value;
+            const text = memeTextInput.value;
+
+            catImage.src = selectedCat;
+            memeText.innerText = text;
+        });
